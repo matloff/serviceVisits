8dfc047dc16d1f91a8d1accce2ca2514f207b494
Norm Matloff <nsmatloff@ucdavis.edu>
2023-06-21 22:43:02 -0700

    a start

---
+++ R/serviceVisits.R
@@ -0,0 +1,5 @@
+
+# assumes two variables that are global to the code being executed:
+
+#    globalObj: the object that is the subject of the "visits"
+#    globalPause, basically an R logical debug/code illustration variable,
