c2b20417934f8e37bd0abf2591986c0c09c72d1b
Norm Matloff <nsmatloff@ucdavis.edu>
2023-06-21 22:48:13 -0700

    a start

--- R/serviceVisits.R
+++ R/serviceVisits.R
@@ -3,3 +3,12 @@
 
 #    globalObj: the object that is the subject of the "visits"
 #    globalPause, basically an R logical debug/code illustration variable,
+
+# 'expr' is quote R code; with semicolons separating statements, e.g.
+# 'ls(); date()'
+
+sCall <- function(expr) 
+{
+   eval(parse(text=expr,parent.frame()))
+   if (globalPause) {}
+}
